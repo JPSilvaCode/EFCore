3850e5e443fcce33452b684748d040494eebc5c5
JOAO PAULO <joao.silva@mgmcorp.com.br>
2021-04-05 10:01:41 -0300

    Add class SwaggerConfig

--- src/EFCWebAPI/Startup.cs
+++ src/EFCWebAPI/Startup.cs
@@ -1,16 +1,9 @@
+using EFCWebAPI.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EFCWebAPI
 {
@@ -26,12 +19,11 @@ namespace EFCWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // WebAPI Config
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EFCWebAPI", Version = "v1" });
-            });
+
+            // Swagger Config
+            services.AddSwaggerConfiguration();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +32,6 @@ namespace EFCWebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EFCWebAPI v1"));
             }
 
             app.UseHttpsRedirection();
@@ -54,6 +44,8 @@ namespace EFCWebAPI
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwaggerSetup();
         }
     }
 }
