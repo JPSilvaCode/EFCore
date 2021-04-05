46827742c878c2215c31fc04febe9c33a96c7833
JOAO PAULO <joao.silva@mgmcorp.com.br>
2021-04-05 11:08:07 -0300

    Add class Context

---
+++ src/EFCData/Context/EFCContext.cs
@@ -0,0 +1,20 @@
+﻿using EFCDomain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCData.Context
+{
+    public sealed class EFCContext : DbContext
+    {
+        public EFCContext(DbContextOptions<EFCContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
